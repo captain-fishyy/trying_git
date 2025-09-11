9d1fb0385943e3ae423ed21665609fab4d24b7fb
caoxy <caoxy2024@shanghaitech.edu.cn>
2025-09-11 12:08:08 +0800

    commands

---
+++ note.c
@@ -0,0 +1,10 @@
+/*
+git --version
+git config --global --list用户名与邮箱
+git config --global user.name "caoxy"
+git config --global user.email ""
+git status
+git branch -m master main
+git init -b main
+git add note.c
+*/
